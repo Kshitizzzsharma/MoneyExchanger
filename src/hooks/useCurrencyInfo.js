332dd38d3f912b5c428baff49f2d1ac3d5aeaf35
kshitizzsharma <kshitizsharma2907@gmail.com>
2024-04-25 15:21:43 +0530

    second

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -6,13 +6,14 @@ function useCurrencyInfo(){
         const [data,setData]= useState({})
         useEffect(()=>{
             fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
-            .then((res)=> res.json)
+            .then((res)=> res.json())
             
-            .then((res)=> console.log(res.toString))
-            // setData(res[currency]))
+            .then((res)=> setData(res[currency]))
+            // 
             
             console.log(data);
         }, [currency])
+        return data
 };
 
  export default useCurrencyInfo ;
